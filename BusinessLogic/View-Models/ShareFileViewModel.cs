d2261a0e752a0ce3c5ef75367a3bd2883943fb29
LoganCamilleri <logan.camilleri.g52818@mcast.edu.mt>
2023-01-15 19:01:38 +0100

    created Edit and share pages, bug fixes

---
+++ BusinessLogic/View-Models/ShareFileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.View_Models
+{
+    public class ShareFileViewModel
+    {
+        public TextFileModel TextFileModel { get; set; }
+        public String Username { get; set; }
+    }
+}
